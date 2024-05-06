f72aaa0ff022dc64924cf311d40d97379b67f19d
mirzayogy <mirza.yogy@gmail.com>
2024-05-06 15:21:02 +0800

    class Mahasiswa

---
+++ Mahasiswa.java
@@ -0,0 +1,25 @@
+public class Mahasiswa{
+    String nama;
+    int umur;
+    String tanggalLahir;
+    double tinggiBadan;
+    String email;
+    String npm;
+    double ipk;
+    int jumlahSks;
+
+    void cetakInformasi(){
+        System.out.println("Nama : " +nama);
+        System.out.println("NPM : " +npm);
+        System.out.println("IPK : " +ipk);
+        System.out.println("jumlahSKS : " +jumlahSks);
+    }
+
+    void menginputData(){
+
+    }
+
+    void cetakKhs(){
+
+    }
+}
