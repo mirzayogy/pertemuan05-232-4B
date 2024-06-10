1ace49afb7af1257f32a6960aaab918f40bb6a58
mirzayogy <mirza.yogy@gmail.com>
2024-06-10 15:57:31 +0800

    overloading

--- CobaFrame.java
+++ CobaFrame.java
@@ -1,6 +1,12 @@
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JFrame;
 
 public class CobaFrame extends JFrame {
+
+    JButton btn1 = new JButton("Tambah");
+    JLabel label1 = new JLabel("Nama ");
+
     public static void main(String[] args) {
         CobaFrame cf = new CobaFrame();
         cf.setSize(600, 480);
--- model/Buku.java
+++ model/Buku.java
@@ -4,6 +4,36 @@ public class Buku {
     private String judul;
     private double harga;
     private String penulis;
+    private double diskon;
+
+    //overloading
+    public Buku(){
+
+    }
+
+    public Buku(String judul){
+        this.judul = judul;
+    }
+
+    public Buku(String judul, double harga) {
+        this.judul = judul;
+        this.harga = harga;
+    }
+
+    public Buku(String judul, double harga, String penulis) {
+        this.judul = judul;
+        this.harga = harga;
+        this.penulis = penulis;
+    }
+
+    public Buku(double diskon, String penulis){
+        this.penulis = penulis;
+        this.diskon = diskon;
+    }
+
+    public Buku(String judul, String penulis){
+
+    }
 
     public void setJudul(String judul){
         this.judul = judul;
@@ -12,4 +42,21 @@ public class Buku {
     public String getJudul(){
         return this.judul;
     }
+
+    //overloading
+    public void hapus(){
+
+    }
+
+    public void hapus(int id){
+
+    }
+
+    public void hapus(String kelas) {
+
+    }
+
+    // public void hapus(String nama) {
+
+    // }
 }
